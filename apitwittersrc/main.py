aebc2e2f565daea0d56950ba143b8ffb3245ff40
Reginaldo Santana <regymatrix@gmail.com>
2023-10-28 20:10:53 -0300

    projeto

---
+++ apitwittersrc/main.py
@@ -0,0 +1,19 @@
+#pip install poetry (Gerenciador de pacotes)
+import requests
+
+from dadosacesso import ACCESS_TOKEN, ACCESS_TOKEN_SECRET, BASE_TOKEN, CONSUMER_KEY, CONSUMER_SECRET
+import tweepy
+
+BRAZIL_WOE_ID= 23424768
+
+auth = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+#trends = api.get_place_trends(BRAZIL_WOE_ID)
+
+tweets = api.request('https://api.twitter.com/2/tweets')
+
+
+for tweet in tweets:
+    print(tweet)
